519f30072ee6fdb27b83899e520a74144d51f0e7
Eladio Linares <eladio.linares@gmail.com>
2020-10-04 09:40:04 +0200

    change test

--- test/exun_test.exs
+++ test/exun_test.exs
@@ -4,21 +4,18 @@ defmodule ExunTest do
 
   test "Parse x[me]/3[se]" do
     assert Exun.parse("x[me]/3[se]") ==
-               {{:divi, {:unit, {:vari, "x"}, {:vari, "me"}}, {:unit, {:numb, 3}, {:vari, "se"}}},
-                %{}}
-
+             {{:unit, {:divi, {:vari, "x"}, {:numb, 3}}, {:divi, {:vari, "me"}, {:vari, "se"}}},
+              %{}}
   end
 
-  test "Parse x[me]/3[se] with %{x => 7, me => m^2, se => s^2}" do
+  test "Parse x[me]/3[se] with %{x => \"7\", me => m^2, se => s^2}" do
     {tree, _expand} =
       Exun.parse(
         "x[me]/3[se]",
-        %{"x" => 7, "me" => "m^2", "se" => "s^2"}
+        %{"x" => "7", "me" => "m^2", "se" => "s^2"}
       )
 
-    assert tree ==  {:divi,
-                      {:unit, {:numb, 7}, {:elev, {:vari, "m"}, {:numb, 2}}},
-                      {:unit, {:numb, 3}, {:elev, {:vari, "s"}, {:numb, 2}}}
-                    }
+    assert tree ==
+      {:unit, {:divi, {:vari, "x"}, {:numb, 3}}, {:divi, {:vari, "me"}, {:vari, "se"}}}
   end
 end
