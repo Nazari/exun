8315dbc15cc8096b5d290fb311408ac9d12f52ad
Nazari <eladiolinares@gmail.com>
2020-10-12 21:53:31 +0200

    Added hex_docs

--- mix.exs
+++ mix.exs
@@ -23,8 +23,7 @@ defmodule Exun.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
@@ -37,10 +36,8 @@ defmodule Exun.MixProject do
       # This option is only needed when you don't want to use the OTP application name
       name: "exun",
       # These are the default files included in the package
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* CHANGELOG* changelog* src),
       licenses: ["Apache-2.0"],
-      links: %{"GitHub" => "https://github.com/elixir-ecto/postgrex"}
+      links: %{"GitHub" => "https://github.com/Nazari/exun"}
     ]
 
   end
