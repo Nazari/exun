c097180adb86910431184b28b3b7ce65a366ba1a
Nazari <eladiolinares@gmail.com>
2020-10-24 13:32:11 +0200

    v0.3.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exun.MixProject do
   def project do
     [
       app: :exun,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.10",
       description: descripcion(),
       start_permanent: Mix.env() == :prod,
