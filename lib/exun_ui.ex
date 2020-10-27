7a47c8dce77ac8dc3ae42ca271bbbb7e370b8c80
Nazari <eladiolinares@gmail.com>
2020-10-27 18:07:59 +0100

    Completely rewritten

--- lib/exun_ui.ex
+++ lib/exun_ui.ex
@@ -17,111 +17,137 @@ defmodule Exun.UI do
   ```
   """
   def tostr(tree) do
-    {txt, _, d} = show(tree)
+    {t, _, d, s} =
+      show(tree)
+      #|> IO.inspect(label: "root show")
 
     cond do
-      String.starts_with?(txt, "/") -> "1" <> txt
-      d -> "1/" <> txt
-      true -> txt
+      d and s -> "-1/#{t}"
+      d and !s -> "1/#{t}"
+      !d and s -> "-#{t}"
+      !d and !s -> t
     end
   end
 
+  @doc """
+  Prints in human readable form an AST.
+  Each returns the text converted, the prioriti of the node, a flag
+  indicating if must be included in denominator of a division, and
+  another flag indicatin if must be preceded with a sign - before expression
+  """
   def show({:numb, n}),
-    do: {to_string(n), 200, false}
+    do: {to_string(n), 200, false, false}
 
   def show({:vari, v}),
-    do: {v, 200, false}
+    do: {v, 200, false, false}
 
   def show({:unit, n, m}) do
-    {t, p, _} = show(n)
-    {tt, _, _} = show(m)
-    {"#{t}[#{tt}]", p, false}
+    {t, p, _, s} = show(n)
+    tt = tostr(m)
+    {"#{t}[#{tt}]", p, false, s}
   end
 
   def show({:minus, a}) do
-    {t, p, _} = show(a)
-
-    if p < 200,
-      do: {"-(#{t})", p, false},
-      else: {"-#{t}", p, false}
+    {t, p, d, s} = show(a)
+    # Xor sign with returned sign s
+    ns = if s, do: false, else: true
+    {withpar(p, 200, t), p, d, ns}
   end
 
   def show({:elev, b, {:numb, nn}}) when nn < 0 do
-    {t, p, _} = show({:elev, b, {:numb, -nn}})
-    {t, p, true}
+    {t, p, d, s} = show({:elev, b, {:numb, -nn}})
+    # Xor denomi with returned denomi
+    nd = if d, do: false, else: true
+    {t, p, nd, s}
   end
 
   def show({:elev, b, {:minus, a}}) do
-    {t, p, _} = show({:elev, b, a})
-    {t, p, true}
+    {t, p, d, s} = show({:elev, b, a})
+    # Xor denomi with returned denomi
+    nd = if d, do: false, else: true
+    {t, p, nd, s}
   end
 
   def show({:elev, b, e}) do
-    {tb, pb, _} = show(b)
-    {te, pe, _} = show(e)
+    {tb, pb, d, s} = show(b)
+    {_, pe, _, _} = show(e)
+    te = tostr(e)
+    tbase = withpar(pb, 200, tb)
+    texpo = withpar(pe, 200, te)
 
     cond do
-      pb < 200 and e == {:numb, 1} -> {"(#{tb})", pb, false}
-      e == {:numb, 1} -> {"#{tb}", pb, false}
-      pe < 200 -> {"#{tb}^(#{te})", pb, false}
-      true -> {"#{tb}^#{te}", pb, false}
+      e == {:numb, 1} -> {"#{tbase}", pb, d, s}
+      true -> {"#{tbase}^#{texpo}", pb, d, s}
     end
   end
 
   def show({:deriv, f, v}) do
-    {tf, pf, _} = show(f)
-    {tv, _, _} = show(v)
+    {tf, pf, d, s} = show(f)
+    tv = tostr(v)
 
-    if pf < 200,
-      do: {"(#{tf})'#{tv}", pf, false},
-      else: {"#{tf}'#{tv}", pf, false}
+    tderi = withpar(pf, 200, tf)
+    {"#{tderi}'#{tv}", pf, d, s}
   end
 
   def show({:integ, f, v}) do
-    {tf, pf, _} = show(f)
-    {tv, _, _} = show(v)
-
-    if pf < 200,
-      do: {"($#{tf},#{tv})", pf, false},
-      else: {"$#{tf},#{tv}", pf, false}
+    {tf, pf, d, s} = show(f)
+    tv = tostr(v)
+    tfinteg = withpar(pf, 200, tf)
+    {"$#{tfinteg},#{tv}", pf, d, s}
   end
 
   def show({:fcall, n, a}) do
     targs =
       Enum.reduce(a, [], fn arg, ltxt ->
-        {t, _, _} = show(arg)
+        t = tostr(arg)
         [t | ltxt]
       end)
       |> Enum.reverse()
       |> Enum.join(",")
 
-    {"#{n}(#{targs})", 200, false}
+    {"#{n}(#{targs})", 200, false, false}
   end
 
   def show({{:m, op}, lst}) do
     lst = Enum.sort(lst, &smm(&1, &2))
-    {opt, pri} = if op == :suma, do: {"+", 50}, else: {"*", 90}
+    {_opt, pri} = if op == :suma, do: {"+", 50}, else: {"*", 90}
 
     texto =
-      Enum.map(lst, fn opand ->
-        {t, p, d} = show(opand)
+      Enum.map(lst, &show(&1))
+      |> Enum.sort(&sop/2)
+      |> Enum.reduce("", fn {t, p, d, s}, atxt ->
+        wp = withpar(p, pri, t)
 
-        cond do
-          d and op == :suma -> "1/#{t}"
-          d and op == :mult -> "/#{t}"
-          p < pri -> "(#{t})"
-          true -> "#{t}"
+        if atxt == "" do
+          if s, do: "-" <> wp, else: wp
+        else
+          suma = op == :suma
+
+          cond do
+            d and s and suma -> "#{atxt}-1/#{wp}"
+            d and s and !suma -> "#{atxt}/-#{wp}"
+            d and !s and suma -> "#{atxt}+1/#{wp}"
+            d and !s and !suma -> "#{atxt}/#{wp}"
+            !d and s and suma -> "#{atxt}-#{wp}"
+            !d and s and !suma -> "#{atxt}*-#{wp}"
+            !d and !s and suma -> "#{atxt}+#{wp}"
+            !d and !s and !suma -> "#{atxt}*#{wp}"
+          end
         end
       end)
-      # |> IO.inspect(label: "map")
-      |> Enum.sort(&sop/2)
-      |> Enum.join(opt)
-      |> String.replace("*/", "/")
 
-    {texto, pri, false}
+    {texto, pri, false, false}
   end
 
-  defp sop(a, _b) do
-    if String.starts_with?(a, "/"), do: false, else: true
+  def sop({_, p1, d1, s1}, {_, p2, d2, s2}) do
+    cond do
+      d1 and !d2 -> false
+      !d1 and d2 -> true
+      s1 and !s2 -> false
+      !s1 and s2 -> true
+      true -> p1 > p2
+    end
   end
+
+  defp withpar(p, pmax, t), do: if p < pmax, do: "(#{t})", else: "#{t}"
 end
